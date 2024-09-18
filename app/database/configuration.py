19d2173faffd7db5a2d42c5c305b4fd749bc5c4f
arlex marin <arlexmarin.armagi@hotmail.com>
2024-09-17 20:34:15 -0500

    se crean tablas en la BD

--- app/database/configuration.py
+++ app/database/configuration.py
@@ -1,4 +1,13 @@
+
+#persistencia es manejar la bd desde el front
+# ORM es el lenguaje para conectar con l bd
+
 #datos para la conexion a BD
+
+from sqlalchemy import create_engine,event
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+
 
 dataBaseName="gestordb"
 userName="root"
@@ -9,4 +18,10 @@ server="localhost"
 #creando la conexion
 dataBaseConnection=f"mysql+mysqlconnector://{userName}:{userPassword}@{server}:{connectionPort}/{dataBaseName}"
 
-print(dataBaseConnection)
+# creanddo el motor de coneccion
+engine = create_engine(dataBaseConnection)
+
+#abrir la sesion con la bd
+
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
